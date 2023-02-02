6711781b8a273b70a5b1442d3ee5b4487ae71298
Wes Hinsley <wes.hinsley@gmail.com>
2023-02-02 21:24:09 +0000

    Sort of default param for solve

--- 2022/Rust/src/d01/mod.rs
+++ 2022/Rust/src/d01/mod.rs
@@ -1,6 +1,10 @@
 use crate::tools;
 
-pub fn solve(file : &str) -> (u32, u32) {
+pub fn solve() -> (u32, u32) {
+    _solve("../inputs/d01-input.txt")
+}
+
+fn _solve(file : &str) -> (u32, u32) {
     let mut elves = Vec::new();
 
     for elf in tools::read_file_contents(file).split("\n\n") {
@@ -18,12 +22,14 @@ pub fn solve(file : &str) -> (u32, u32) {
     (elves[0], elves[0] + elves[1] + elves[2])
 }
 
+
+
 #[cfg(test)]
 mod tests {
     use crate::*;
     #[test]
     fn test_day1() {
-        let res = d01::solve("../inputs/d01-test.txt");
+        let res = d01::_solve("../inputs/d01-test.txt");
         assert_eq!(res.0, 24000);
         assert_eq!(res.1, 45000);
     }
--- 2022/Rust/src/main.rs
+++ 2022/Rust/src/main.rs
@@ -2,6 +2,6 @@ mod tools;
 mod d01;
 
 fn main() {
-  tools::print_day(d01::solve("../inputs/d01-input.txt"));
+  tools::print_day(d01::solve());
 }
 
