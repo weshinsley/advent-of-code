e379ceba0909f16256a9815ff9af02b463e3a1e8
weshinsley <w.hinsley@imperial.ac.uk>
2023-02-02 10:19:47 +0000

    Better file read

--- 2022/Rust/src/bin/d01.rs
+++ 2022/Rust/src/bin/d01.rs
@@ -1,12 +1,12 @@
-fn solve() -> (u32, u32) {
+fn solve(file : &str) -> (u32, u32) {
   
-  let input = std::include_str!("../../../inputs/d01-input.txt");
+  let input = std::fs::read_to_string(file).expect("Fatal Elf Error");
   let mut elves = Vec::new();
 
   for elf in input.split("\n\n") {
     let mut elf_total: u32 = 0;
     for weight in elf.split("\n") {
-      if weight != "" {
+      if weight!= "" {
         let int_weight: u32 = weight.parse().unwrap();
         elf_total += int_weight;
        }
@@ -20,7 +20,8 @@ fn solve() -> (u32, u32) {
 }
 
 fn main() {
-  let res = solve();
+  let res = solve("../../../inputs/d01-input.txt");
   println!("Part 1: {}", res.0);
   println!("Part 2: {}", res.1);
 }
+
