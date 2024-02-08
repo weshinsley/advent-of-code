3532c7612a6f0505f6b3a4e4cccef7a1a348e9fc
weshinsley <w.hinsley@imperial.ac.uk>
2024-02-08 22:54:18 +0000

    2023, Day 17, slow R

--- 2023/R/d17.R
+++ 2023/R/d17.R
@@ -1,113 +1,152 @@
-# TO-DO - this one is broken!
-
 parse_input <- function(f) {
   d <- readLines(f)
   matrix(as.integer(strsplit(paste0(d, collapse = ""), "")[[1]]),
          nrow = length(d), ncol = nchar(d[[1]]), byrow = TRUE)
 }
 
-part <- function(d) {
-  state <- list(x = 1, y = 1, steps = 0, )
-}
+go <- function(d, min_steps, max_steps, wid = ncol(d), hei = nrow(d)) {
+  visited <- array(FALSE, c(wid, hei, 4L, max_steps + 1L))
+  pq_capacity <- 100000
+  dx <- c(1L, 0L, -1L, 0L)
+  dy <- c(0L, 1L, 0L, -1L)
+  i <- 1L
+  pq_x <- rep(-1L, pq_capacity)
+  pq_y <- rep(-1L, pq_capacity)
+  pq_d <- rep(-1L, pq_capacity)
+  pq_h <- rep(-1L, pq_capacity)
+  pq_s <- rep(-1L, pq_capacity)
+  pq_n <- rep(-1L, pq_capacity)
 
+  # Initialise with first states - top-left, no steps or heat.
 
-part1 <- function(d) {
-  wid <- ncol(d)
-  hei <- nrow(d)
-  dx <- c(1, 0, -1, 0)
-  dy <- c(0, 1, 0, -1)
+  pq_x[1L] <- 1L
+  pq_y[1L] <- 1L
+  pq_d[1L] <- 1L
+  pq_s[1L] <- 0L
+  pq_h[1L] <- 0L
+  pq_n[1L] <- -1L
 
-  best <- 0
-  top1 <- 1:wid
-  top1 <- top1[top1 %% 4 != 0]
-  top2 <- top1 + 2
-  top2 <- top2[top2 <= wid]
-  best <- sum(d[1, top1]) + sum(d[2, top2]) + sum(d[wid, top1]) + sum(d[wid - 1, top2])
+  next_slot <- 2L
 
- # for (i in 1:wid) {
-#    best <- best + d[i, i]
-#    if (i < wid) best <- best + d[i, i + 1]
-#  }
-  message(best)
-  best <- min(750, best)
-  message(best)
-  bestm <- (d * 0) + best
-  bestm <- list(bestm, bestm, bestm)
+  while(i != -1L)  {
 
-  work <- list(list(x = 1, y = 1, dir = 1, steps = 0, tot = 0))
-  at <- 1
-  len <- 1
-  while (at <= len) {
-    here <- work[[at]]
-    at <- at + 1
-    #work[[1]] <- NULL
-    #len <- len - 1
-    if ((here$tot >= best) || (here$x < 1) || (here$y < 1) ||
-        (here$x > wid) || (here$y > hei)) {
+    if (visited[pq_x[i], pq_y[i], pq_d[i], 1L + pq_s[i]]) {
+      i <- pq_n[i]
       next
     }
 
-    if (here$tot >= bestm[[here$steps + 1]][here$y, here$x]) {
-      next
-    }
-    bestm[[here$steps + 1]][here$y, here$x] <- here$tot
-    if ((here$x == wid) && (here$y == hei)) {
-      best <- min(best, here$tot)
-      message(best)
-      next
+    visited[pq_x[i], pq_y[i], pq_d[i], 1L + pq_s[i]] <- TRUE
+
+    # Got there?
+
+    if (pq_x[i] == wid && pq_y[i] == hei && pq_s[i] >= min_steps) {
+      return(pq_h[i])
     }
 
-    if (here$steps < 2) {
-      if ((here$x + dx[here$dir] >= 1) && (here$x + dx[here$dir] <= wid) &&
-          (here$y + dy[here$dir] >= 1) && (here$y + dy[here$dir] <= hei)) {
-        newtot <- here$tot + d[here$y + dy[here$dir], here$x + dx[here$dir]]
-        if (newtot < best) {
+    # Explore going straight on.
 
-          len <- len + 1
-          work[[len]] <- list(x = here$x + dx[here$dir], y = here$y + dy[here$dir],
-                            dir = here$dir, steps = here$steps + 1,
-                            tot = newtot)
+    if (pq_s[i] < max_steps - 1L) {
+      nx <- pq_x[i] + dx[pq_d[i]]
+      ny <- pq_y[i] + dy[pq_d[i]]
+      if (nx >= 1L && nx <= wid && ny >= 1L && ny <= hei) {
+        nh <- pq_h[i] + d[ny, nx]
+        if (!visited[nx, ny, pq_d[i], 2L + pq_s[i]]) {
+          prev <- i
+          while (TRUE) {
+            if (pq_n[prev] == -1L) {
+              pq_n[prev] <- next_slot
+              pq_x[next_slot] <- nx
+              pq_y[next_slot] <- ny
+              pq_d[next_slot] <- pq_d[i]
+              pq_s[next_slot] <- 1L + pq_s[i]
+              pq_h[next_slot] <- nh
+              pq_n[next_slot] <- -1L
+              next_slot <- next_slot + 1L
+              break
+            }
+            nxtp <- pq_n[prev]
+            if (pq_h[nxtp] < nh) {
+              prev <- nxtp
+              next
+            }
+            pq_n[prev] <- next_slot
+            pq_x[next_slot] <- nx
+            pq_y[next_slot] <- ny
+            pq_d[next_slot] <- pq_d[i]
+            pq_s[next_slot] <- 1L + pq_s[i]
+            pq_h[next_slot] <- nh
+            pq_n[next_slot] <- nxtp
+            next_slot <- next_slot + 1L
+            break
+          }
         }
       }
     }
 
-    here$dir <- (here$dir %% 4) + 1
-    if ((here$x + dx[here$dir] >= 1) && (here$x + dx[here$dir] <= wid) &&
-        (here$y + dy[here$dir] >= 1) && (here$y + dy[here$dir] <= hei)) {
-      newtot <- here$tot + d[here$y + dy[here$dir], here$x + dx[here$dir]]
-      if (newtot < best) {
-        len <- len + 1
-        work[[len]] <- list(x = here$x + dx[here$dir], y = here$y + dy[here$dir],
-                              dir = here$dir, steps = 0,
-                              tot = newtot)
+    # Explore turning left/right
+
+    nd <- pq_d[i]
+    if (pq_s[i] >= min_steps) {
+      for (x in c(1L, 2L)) {
+        nd <- nd + x
+        if (nd > 4L) nd <- nd - 4L
+        nx <- pq_x[i] + dx[nd]
+        ny <- pq_y[i] + dy[nd]
+
+        if (nx >= 1L && nx <= wid && ny >= 1L && ny <= hei) {
+          nh <- pq_h[i] + d[ny, nx]
+          if (!visited[nx, ny, nd, 1L]) {
+            prev <- i
+            while (TRUE) {
+              if (pq_n[prev] == -1L) {
+                pq_n[prev] <- next_slot
+                pq_x[next_slot] <- nx
+                pq_y[next_slot] <- ny
+                pq_s[next_slot] <- 0L
+                pq_h[next_slot] <- nh
+                pq_d[next_slot] <- nd
+                pq_n[next_slot] <- -1L
+                next_slot <- next_slot + 1L
+                break
+              }
+              nxtp <- pq_n[prev]
+              if (pq_h[nxtp] < nh) {
+                prev <- nxtp
+                next
+              }
+              pq_n[prev] <- next_slot
+              pq_x[next_slot] <- nx
+              pq_y[next_slot] <- ny
+              pq_d[next_slot] <- nd
+              pq_s[next_slot] <- 0L
+              pq_h[next_slot] <- nh
+              pq_n[next_slot] <- nxtp
+              next_slot <- next_slot + 1L
+              break
+            }
+          }
+        }
       }
     }
-
-    here$dir <- (here$dir %% 4) + 1
-    here$dir <- (here$dir %% 4) + 1
-    if ((here$x + dx[here$dir] >= 1) && (here$x + dx[here$dir] <= wid) &&
-        (here$y + dy[here$dir] >= 1) && (here$y + dy[here$dir] <= hei)) {
-      newtot <- here$tot + d[here$y + dy[here$dir], here$x + dx[here$dir]]
-      if (newtot < best) {
-        len <- len + 1
-
-        work[[len]] <- list(x = here$x + dx[here$dir], y = here$y + dy[here$dir],
-                          dir = here$dir, steps = 0,
-                          tot = newtot)
-      }
-    }
+    i <- pq_n[i]
   }
-  best
+  9999
+}
+
+part1 <- function(d) {
+  go(d, 0L, 3L)
 }
 
 part2 <- function(d) {
-  0
+  go(d, 3L, 10L)
 }
 
 test <- function() {
   d <- parse_input("../inputs/d17-test.txt")
   stopifnot(part1(d) == 102)
-  #stopifnot(part2(d) == 51)
+  stopifnot(part2(d) == 94)
+  d <- parse_input("../inputs/d17-test2.txt")
+  stopifnot(part2(d) == 71)
 }
 
 test()
